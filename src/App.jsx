1f55178a0d6ce028835126fdb9134be8eee9e9e3
AroshXR <aroshanasandeep454@gmail.com>
2026-05-01 23:23:11 +0530

    Exercise 02: Implement NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import Greeting from './components/Greeting'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="app-container">
+      <NavBar />
       <Greeting />
     </div>
   )
